3f257b7d0d81bebbe9bee4005bc04f192c95b079
niranjana687 <niranjana687@gmail.com>
2021-09-22 17:26:30 +0530

    hbs error fixed

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
-const { AppBar } = require('@material-ui/core');
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 
-const app = express.app();
+const app = express();
 const port = 3000;
 
 //paths
-const publicDirectoryPath = path(__dirname, '../public');
-const viewsPath = path(__dirname, '../templates/views');
-const partialsPath = path(__dirname, '../templates/partials');
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 //view engine
-app.set('view engine', hbs);
-app.set('viewS', viewsPath);
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
 hbs.registerPartials(partialsPath);
 
 //setting up static directory
@@ -22,8 +21,23 @@ app.use(express.static(publicDirectoryPath));
 //home page 
 app.get('', (request, response) => {
     response.render('index');
-})
+});
+
+//team
+app.get('/team', (request, response) => {
+    response.render('team');
+});
+
+//impact
+app.get('/impact', (requesr, response) => {
+    response.render('impact');
+});
+
+//contact 
+app.get('/contact', (request, response) => {
+    response.render('contact');
+});
 
 app.listen(port, () => {
     console.log(`Listening at localhost:${port}`);
-})
+});
